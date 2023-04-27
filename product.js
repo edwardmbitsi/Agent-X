4727be93660140c02b10ac2b4034ec21394acd36
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2023-04-28 02:28:39 +0300

    Add files via upload

---
+++ product.js
@@ -0,0 +1,183 @@
+const products = [
+    {
+        id: 0,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Runda',
+        profession: 'Nurse',
+        qualifications: 'degree'
+
+        
+    },
+    {
+        id: 1,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Kiambu Road',
+        profession: 'Nurse',
+        qualifications: 'degree'
+
+
+    },
+    {
+        id: 2,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Kilimani',
+        profession: 'Nurse',
+        qualifications: 'degree'
+
+    },
+    {
+        id: 3,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Westlands',
+        profession: 'Nurse',
+        qualifications: 'degree'
+
+    },
+    {
+        id: 4,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Karen',
+        profession: 'Nurse',
+        qualifications: 'degree'
+
+    },
+    {
+        id: 5,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Runda',
+        profession: 'Nurse',
+        qualifications: 'degree'
+
+    },
+    {
+        id: 6,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Lavington',
+        profession: 'Nurse',
+        qualifications: 'degree'
+        
+    },
+    {
+        id: 7,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Kitengela',
+        profession: 'Nurse',
+        qualifications: 'degree'
+
+    },
+    {
+        id: 8,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Runda',
+        profession: 'Nurse',
+        qualifications: 'degree'
+
+    },
+    {
+        id: 9,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Karen',
+        profession: 'Nurse',
+        qualifications: 'degree'
+
+    },
+    {
+        id: 10,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Kiambu Road',
+        profession: 'Writer',
+        qualifications: 'diploma'
+    },
+    {
+        id: 11,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Kilimani',
+        profession: 'Driver',
+        qualifications: 'certificate'
+    },
+    {
+        id: 12,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Spring Valley',
+        profession: 'Maid',
+        qualifications: 'certificate'
+    },
+    {
+        id: 13,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Muthaiga',
+        profession: 'Carpenter',
+        qualifications: 'Certificate'
+    },
+    {
+        id: 14,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Gigiri',
+        profession: 'Sales',
+        qualifications: 'degree'
+    },
+    {
+        id: 15,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Kilimani',
+        profession: 'Driver',
+        qualifications: 'degree'
+    },
+    {
+        id: 16,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Muthaiga',
+        profession: 'Maid',
+        qualifications: 'diploma'
+    },
+    {
+        id: 17,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Muthaiga',
+        profession: 'Carpenter',
+        qualifications: 'Certificate'
+    },
+    {
+        id: 18,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Spring Valley',
+        profession: 'Writer',
+        qualifications: 'diploma'
+    },
+    {
+        id: 19,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Spring Valley',
+        profession: 'Nurse',
+        qualifications: 'degree'
+    },
+    {
+        id: 20,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Lavington',
+        profession: 'Nurse',
+        qualifications: 'degree'
+    },
+    {
+        id: 21,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Westlands',
+        profession: 'Nurse',
+        qualifications: 'degree'
+    },
+    {
+        id: 22,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Karen',
+        profession: 'Nurse',
+        qualifications: 'degree'
+    },
+    {
+        id: 23,
+        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        location: 'Kiambu Road',
+        profession: 'Nurse',
+        qualifications: 'degree'
+    },
+    
+]
