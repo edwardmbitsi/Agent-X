0f060f001683851d341e14f3d1c5a4344e554746
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2023-05-01 20:39:37 +0300

    Update product.js

--- product.js
+++ product.js
@@ -22,6 +22,8 @@ const products = [
         imgSrc : 'img/81.png',
         location: 'Watamu',
         profession: 'Ready Title',
+        qualifications: '2 acres'
+
 
     },
     {
@@ -29,6 +31,8 @@ const products = [
         imgSrc : 'img/82.png',
         location: 'Malindi South',
         profession: 'Ready Title',
+        qualifications: '2 acres'
+
 
     },
     {
@@ -36,6 +40,7 @@ const products = [
         imgSrc : 'img/83.png',
         location: 'Kijiwetanga',
         profession: 'Ready Title',
+        qualifications: '2 acres'
 
     },
     {
@@ -43,21 +48,24 @@ const products = [
         imgSrc : 'img/84.png',
         location: 'Watamu',
         profession: 'Ready Title',
+        qualifications: '2 acres'
 
     },
     {
         id: 6,
-        imgSrc : 'img/85.png',
-        location: 'Kilifi',
+        imgSrc : 'img/86.png',
+        location: 'Bore Singwaya',
         profession: 'Ready Title',
+        qualifications: '30 acres'
 
         
     },
     {
         id: 7,
-        imgSrc : 'https://images.pexels.com/photos/5668859/pexels-photo-5668859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        location: 'Watamu',
+        imgSrc : 'img/87.png',
+        location: 'Magarini, ShauriMoyo',
         profession: 'Ready Title',
+        qualifications: '33 acres'
 
     },
    ]
